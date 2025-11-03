05714af8a6f3224aeb9760682b74cdac329ed181
Hugo <150777224+tehisaru@users.noreply.github.com>
2025-11-03 18:20:53 +0000

    changes image path

--- client/src/components/Menu/MainMenu.tsx
+++ client/src/components/Menu/MainMenu.tsx
@@ -538,7 +538,7 @@ const MainMenu: React.FC = () => {
                   <div className="absolute inset-0 flex items-center justify-center">
                     {config.control === PLAYER_CONTROL.HUMAN ? (
                       <img 
-                        src="/icons/human_icon.png" 
+                        src="/Base-Reaction-2/client/public/icons/human_icon.svg" 
                         alt="Human" 
                         className="w-7 h-7"
                         loading="eager"
@@ -546,7 +546,7 @@ const MainMenu: React.FC = () => {
                       />
                     ) : (
                       <img 
-                        src="/icons/AI_icon.png" 
+                        src="/Base-Reaction-2/client/public/icons/AI_icon.svg" 
                         alt="AI" 
                         className="w-7 h-7"
                         loading="eager"
